17b4969b9ef7ee44efc8e1281044e67a0977f10c
Seerban <serbanandrei1337@gmail.com>
2025-12-07 13:41:39 +0200

    Fixed anchor to moving objects

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -4,6 +4,8 @@ public class Hand : MonoBehaviour
 {
     private Rigidbody rb;
     public bool isAnchored = false;
+    private GameObject collObj; // object hand is anchored to, used for moving platforms
+    private Transform tracker; // position that follows platform
 
     void Start()
     {
@@ -12,8 +14,14 @@ public class Hand : MonoBehaviour
 
     void OnCollisionStay(Collision collision)
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) {
             isAnchored = true;
+
+            collObj = collision.gameObject;
+            tracker = new GameObject("Tracker").transform;
+            tracker.SetParent(collObj.transform);
+            tracker.position = transform.position;
+        }
     }
 
     void FixedUpdate()
@@ -21,12 +29,16 @@ public class Hand : MonoBehaviour
         if (isAnchored && Input.GetMouseButton(0))
         {
             rb.linearVelocity = Vector3.zero;
-            gameObject.GetComponent<Renderer>().material.color = Color.red;
+            gameObject.GetComponent<Renderer>().material.color = Color.gray;
+            transform.position = tracker.position;
+            transform.rotation = tracker.rotation;
         }
         else
         {
+            gameObject.GetComponent<Renderer>().material.color = Color.white; // reset color
             isAnchored = false;
-            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            collObj = null; // free reference and tracker
+            tracker = null;
         }   
         
     }
